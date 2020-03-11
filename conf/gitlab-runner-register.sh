2ab4f4120ae5b19f59b0d14ea8e931eff8846340
Niky <niky@ubmg.es>
2020-03-11 12:24:00 +0000

    Update gitlab-runner-register.sh

--- conf/gitlab-runner-register.sh
+++ conf/gitlab-runner-register.sh
@@ -14,4 +14,4 @@ docker exec -it gitlab-runner1 \
     --executor docker \
     --docker-image docker:stable \
     --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
-    --docker-network-mode gitlab-network
+    --docker-network-mode gitlab_net
